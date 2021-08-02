185f8402ad561920699b6628a9d2f441a83c3a3d
QuoZer <m.d.panteleev@gmail.com>
2021-08-02 15:16:28 +0300

    added a1sdk (telemetry viewer)

---
+++ src/a1sdk/build/CMakeFiles/a1sdk_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a1sdk_node.dir/src/a1telemetry_node.cpp.o"
+  "CMakeFiles/a1sdk_node.dir/src/a1telemetry_node.cpp.o.d"
+  "devel/lib/a1sdk/a1sdk_node"
+  "devel/lib/a1sdk/a1sdk_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a1sdk_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
