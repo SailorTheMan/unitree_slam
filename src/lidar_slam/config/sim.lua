5c84e2250ec9da3364313284c1ba589c79d8348a
SailorTheMan <vlad.los77712@gmail.com>
2021-08-02 10:41:10 +0300

    cartographer config changed

--- src/lidar_slam/config/sim.lua
+++ src/lidar_slam/config/sim.lua
@@ -36,28 +36,45 @@ options = {
   pose_publish_period_sec = 5e-3,
   trajectory_publish_period_sec = 30e-3,
   rangefinder_sampling_ratio = 1.,
-  odometry_sampling_ratio = 1.,
+  odometry_sampling_ratio = 0.1,
   fixed_frame_pose_sampling_ratio = 1.,
-  imu_sampling_ratio = 0.5,
+  imu_sampling_ratio = 0.1,
   landmarks_sampling_ratio = 1.,
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 MAP_BUILDER.num_background_threads=12
-TRAJECTORY_BUILDER_2D.submaps.num_range_data = 20
-TRAJECTORY_BUILDER_2D.min_range = 0.5
-TRAJECTORY_BUILDER_2D.max_range = 20.
+
+
+TRAJECTORY_BUILDER_2D.min_range = 0.1
+TRAJECTORY_BUILDER_2D.max_range = 3.5
+TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.
+TRAJECTORY_BUILDER_2D.use_imu_data = false
 TRAJECTORY_BUILDER_2D.min_z = -1.0
 TRAJECTORY_BUILDER_2D.max_z = 1.0
-TRAJECTORY_BUILDER_2D.missing_data_ray_length = 25.
-TRAJECTORY_BUILDER_2D.use_imu_data =false
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.15
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 10.
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight = 0.01
+-- TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.15
+-- TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 10.
+-- TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight = 0.01
+TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
 
-POSE_GRAPH.optimization_problem.huber_scale = 1e2
-POSE_GRAPH.optimize_every_n_nodes = 0
 POSE_GRAPH.constraint_builder.min_score = 0.65
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
+
+-- TRAJECTORY_BUILDER_2D.submaps.num_range_data = 20
+-- TRAJECTORY_BUILDER_2D.min_range = 0.5
+-- TRAJECTORY_BUILDER_2D.max_range = 20.
+-- TRAJECTORY_BUILDER_2D.min_z = -1.0
+-- TRAJECTORY_BUILDER_2D.max_z = 1.0
+-- TRAJECTORY_BUILDER_2D.missing_data_ray_length = 25.
+-- TRAJECTORY_BUILDER_2D.use_imu_data =false
+-- TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+-- TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.15
+-- TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 10.
+-- TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight = 0.01
+
+-- POSE_GRAPH.optimization_problem.huber_scale = 1e2
+-- POSE_GRAPH.optimize_every_n_nodes = 0
+-- POSE_GRAPH.constraint_builder.min_score = 0.65
 
 return options
