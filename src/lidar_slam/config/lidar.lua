ee77c96a038a98d6fcf211d22a04584137499020
SailorTheMan <vlad.los77712@gmail.com>
2021-08-12 21:44:12 +0300

    kinda working realsense livox doesn't working

--- src/lidar_slam/config/lidar.lua
+++ src/lidar_slam/config/lidar.lua
@@ -19,12 +19,12 @@ options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",--[[  ]]
-  tracking_frame = "livox_frame",
-  published_frame = "base",
-  odom_frame = "base",
+  tracking_frame = "base",
+  published_frame = "odom",
+  odom_frame = "odom",
   provide_odom_frame = false,
   publish_frame_projected_to_2d = true,
-  use_odometry = false,
+  use_odometry = true,
   use_nav_sat = false,
   use_landmarks = false,
   num_laser_scans = 0,
@@ -35,26 +35,28 @@ options = {
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 5e-3,
   trajectory_publish_period_sec = 30e-3,
-  rangefinder_sampling_ratio = 1.,
-  odometry_sampling_ratio = 1.,
+  rangefinder_sampling_ratio = 0.5,
+  odometry_sampling_ratio = 0.5,
   fixed_frame_pose_sampling_ratio = 1.,
   imu_sampling_ratio = 1.,
   landmarks_sampling_ratio = 1.,
 }
 MAP_BUILDER.use_trajectory_builder_2d = true
-TRAJECTORY_BUILDER_2D.use_imu_data = true
+TRAJECTORY_BUILDER_2D.use_imu_data = false
 POSE_GRAPH.optimize_every_n_nodes = 0
 
-TRAJECTORY_BUILDER_2D.min_z = -0.2
-TRAJECTORY_BUILDER_2D.max_z = 1.0
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 250
+TRAJECTORY_BUILDER_2D.min_z = -0.1
+TRAJECTORY_BUILDER_2D.max_z = 0.1
 TRAJECTORY_BUILDER_2D.min_range = 0.01
 TRAJECTORY_BUILDER_2D.max_range = 20
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 20
+-- TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.5)
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 20
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 15
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 13
 
 
-TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = false
 
 
 
